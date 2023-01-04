77458acddd5d8d36db02eee129fc7a7fe6c19cc2
caixiaomei <2513018185@qq.com>
2023-01-04 10:13:10 +0800

    feat: 添加模板渲染

--- internal/sql2struct/template.go
+++ internal/sql2struct/template.go
@@ -1,5 +1,12 @@
 package sql2struct
 
+import (
+	"fmt"
+	"go-common-tools/internal/word"
+	"os"
+	"text/template"
+)
+
 // structTpl 预定义模板
 const structTpl = `type {{.TableName | ToCamelCase}} struct {
 {{range .Columns}}	{{ $length := len .Comment}} {{ if gt $length 0 }}// {{.Comment}} {{else}}// {{.Name}} {{ end }}
@@ -29,4 +36,36 @@ type StructTemplateDB struct {
 
 func NewStructTemplate() *StructTemplate {
 	return &StructTemplate{structTpl: structTpl}
+}
+
+func (t *StructTemplate) AssemblyColumns(tbColumns []*TableColumn) []*StructColumn {
+	tplColumns := make([]*StructColumn, 0, len(tbColumns))
+	for _, column := range tbColumns {
+		tag := fmt.Sprintf("`"+"json:"+"\"%s\""+"`", column.ColumnName)
+		tplColumns = append(tplColumns, &StructColumn{
+			Name:    column.ColumnName,
+			Type:    DBTypeToStructType[column.DataType],
+			Tag:     tag,
+			Comment: column.ColumnComment,
+		})
+	}
+
+	return tplColumns
+}
+
+func (t *StructTemplate) Generate(tableName string, tplColumns []*StructColumn) error {
+	tpl := template.Must(template.New("sql2struct").Funcs(template.FuncMap{
+		"ToCamelCase": word.UnderscoreToUpperCamelCase,
+	}).Parse(t.structTpl))
+
+	tplDB := StructTemplateDB{
+		TableName: tableName,
+		Columns:   tplColumns,
+	}
+	err := tpl.Execute(os.Stdout, tplDB)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
